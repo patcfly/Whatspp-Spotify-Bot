1eed5a2b35f898ca13c1b288076a2e65f84726b3
Ritwik Patil <draguve@gmail.com>
2020-03-21 21:02:27 +0530

    Added login and can read messages

--- whatsapp.go
+++ whatsapp.go
@@ -1,21 +1,143 @@
 package main
+
 import (
+	"encoding/gob"
 	"fmt"
-	"github.com/Rhymen/go-whatsapp"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
+	"strings"
+
+	qrcodeTerminal "github.com/Baozisoftware/qrcode-terminal-go"
+	"github.com/Rhymen/go-whatsapp"
 )
 
-
-
-
-func main() {
-	wac, _ := whatsapp.NewConn(20 * time.Second)
-	qrChan := make(chan string)
-	go func() {
-		b64LoginToken := <-qrChan
-		fmt.Printf("qr code: %v\n",b64LoginToken)
-		//show qr code or save it somewhere to scan
-	}()
-	wac.Login(qrChan)
+type waHandler struct {
+	c *whatsapp.Conn
 }
 
+//HandleError needs to be implemented to be a valid WhatsApp handler
+func (h *waHandler) HandleError(err error) {
+
+	if e, ok := err.(*whatsapp.ErrConnectionFailed); ok {
+		log.Printf("Connection failed, underlying error: %v", e.Err)
+		log.Println("Waiting 30sec...")
+		<-time.After(30 * time.Second)
+		log.Println("Reconnecting...")
+		err := h.c.Restore()
+		if err != nil {
+			log.Fatalf("Restore failed: %v", err)
+		}
+	} else {
+		log.Printf("error occoured: %v\n", err)
+	}
+}
+
+func (*waHandler) HandleTextMessage(message whatsapp.TextMessage) {
+	splitData := strings.FieldsFunc(message.Info.RemoteJid, Split)
+	if splitData[1] == "1500829488" {
+		fmt.Printf(message.Text + "\n")
+	}
+}
+
+func Split(r rune) bool {
+	return r == '-' || r == '@'
+}
+
+func main() {
+	//create new WhatsApp connection
+	wac, err := whatsapp.NewConn(5 * time.Second)
+	if err != nil {
+		log.Fatalf("error creating connection: %v\n", err)
+	}
+
+	//Add handler
+	wac.AddHandler(&waHandler{wac})
+
+	//login or restore
+	if err := login(wac); err != nil {
+		log.Fatalf("error logging in: %v\n", err)
+	}
+
+	//verifies phone connectivity
+	pong, err := wac.AdminTest()
+
+	if !pong || err != nil {
+		log.Fatalf("error pinging in: %v\n", err)
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	//Disconnect safe
+	fmt.Println("Shutting down now.")
+	session, err := wac.Disconnect()
+	if err != nil {
+		log.Fatalf("error disconnecting: %v\n", err)
+	}
+	if err := writeSession(session); err != nil {
+		log.Fatalf("error saving session: %v", err)
+	}
+}
+
+func login(wac *whatsapp.Conn) error {
+	//load saved session
+	session, err := readSession()
+	if err == nil {
+		//restore session
+		session, err = wac.RestoreWithSession(session)
+		if err != nil {
+			return fmt.Errorf("restoring failed: %v\n", err)
+		}
+	} else {
+		//no saved session -> regular login
+		qr := make(chan string)
+		go func() {
+			terminal := qrcodeTerminal.New()
+			terminal.Get(<-qr).Print()
+		}()
+		session, err = wac.Login(qr)
+		if err != nil {
+			return fmt.Errorf("error during login: %v\n", err)
+		}
+	}
+
+	//save session
+	err = writeSession(session)
+	if err != nil {
+		return fmt.Errorf("error saving session: %v\n", err)
+	}
+	return nil
+}
+
+func readSession() (whatsapp.Session, error) {
+	session := whatsapp.Session{}
+	file, err := os.Open(os.TempDir() + "/whatsappSession.gob")
+	if err != nil {
+		return session, err
+	}
+	defer file.Close()
+	decoder := gob.NewDecoder(file)
+	err = decoder.Decode(&session)
+	if err != nil {
+		return session, err
+	}
+	return session, nil
+}
+
+func writeSession(session whatsapp.Session) error {
+	file, err := os.Create(os.TempDir() + "/whatsappSession.gob")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	encoder := gob.NewEncoder(file)
+	err = encoder.Encode(session)
+	if err != nil {
+		return err
+	}
+	return nil
+}
